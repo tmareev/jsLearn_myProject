867bb8652bc7dc26500237f59c3a9cab5fc7a8fc
mareev (Тарас Мареев) <mareev@office.local>
2018-09-17 16:20:38 +0300

    temp push

--- ajaxLearning/public/main.js
+++ ajaxLearning/public/main.js
@@ -2,4 +2,21 @@ let registerBtn = document.querySelector('#registerBtn');
 
 registerBtn.addEventListener('click', function(){
     console.log('register clicked');
+   
+});
+registerBtn.addEventListener('click', function(){
+    let temp = {
+        lastname: "Sheva",
+        firstname: "Andr",
+        team: "Milan"
+    }
+    
+    fetch('/players.json')
+    .then(data=>data.json())
+    .then(function(data){
+        console.log(data);
+        data["players"].push(temp);
+        JSON.stringify(data);
+        console.log(data);
+    });
 })
